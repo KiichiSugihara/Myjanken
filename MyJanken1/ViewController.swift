6e33a389f72c809197dd8498a5f1814b9a64dc55
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2018-08-13 13:27:29 +0900

    Janken app作成

---
+++ MyJanken1/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  MyJanken1
+//
+//  Created by Kiichi  on 2018/06/05.
+//  Copyright © 2018年 Kiichi Sugihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    //じゃんけん(数学)
+    var answerNumber = 0
+    
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+        //新しいじゃんけんの結果を一時的に格納する変数を設ける
+        var newAnswerNumber = 0
+        
+        //ランダムに結果を出すが、前回の結果と異なる場合のみ採用
+        
+        repeat{
+            //0,1,2の数値をランダムに算出(乱数)
+            newAnswerNumber = Int(arc4random_uniform(3))
+            
+        } while  answerNumber == newAnswerNumber
+        answerNumber = newAnswerNumber
+        
+        if answerNumber == 0{
+            //グー
+
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named:"gu")
+        }else if answerNumber == 1{
+            // チョキ
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named:"choki")
+        } else if answerNumber == 2 {
+            //パー
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named: "pa")
+            
+        }
+        
+        
+    }
+    
+}
+
